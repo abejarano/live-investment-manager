f52891b595ff21bd6bbd24a681c763586ef1d38b
Angel Bejarano <programador.angel@gmail.com>
2021-12-07 18:22:55 -0300

    [FEAT] Grafico de distribucion de inversion

--- apps/investment/admin.py
+++ apps/investment/admin.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+from admincharts.admin import AdminChartMixin
 from django.contrib import admin
 
 # Register your models here.
@@ -9,7 +12,7 @@ from apps.investment.models import Investment, Capital
 
 
 @admin.register(Investment)
-class AdminInvestment(admin.ModelAdmin):
+class AdminInvestment(AdminChartMixin, admin.ModelAdmin):
     list_display = ['crypto', 'price_crypto', 'precio_actual', 'amount', 'amount_crypto', 'ROI']
     add_form_template = 'investment/form_add_investment.html'
     form = FormInvestment
@@ -40,3 +43,29 @@ class AdminInvestment(admin.ModelAdmin):
                 ).save()
 
         obj.save()
+
+    def get_list_chart_data(self, queryset):
+        if not queryset:
+            return {}
+
+        labels = []
+        totals = []
+        aux = {}
+
+        for crypto_query in queryset:
+            amount = crypto_query.amount
+            if crypto_query.crypto.name in aux.keys():
+                amount = Decimal(aux[crypto_query.crypto.name]) + amount
+
+            aux[crypto_query.crypto.name] = amount
+
+        for crypto_name, amount in aux.items():
+            labels.append(crypto_name)
+            totals.append(amount)
+
+        return {
+            "labels": labels,
+            "datasets": [
+                {"label": "Inversión por cripto", "data": totals, "backgroundColor": "#79aec8"},
+            ],
+        }
