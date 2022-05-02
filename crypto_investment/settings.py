ecc2752b39503318a6fa0fb2452a6acc5a3b1533
abejarano <programador.angel@gmail.com>
2022-05-02 16:39:23 -0300

    [FEAT] logs de errores.

--- crypto_investment/settings.py
+++ crypto_investment/settings.py
@@ -139,3 +139,37 @@ STATIC_URL = '/static/'
 # )
 # print(STATICFILES_DIRS)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if not DEV:
+
+    LOGGING = {
+        'version': 1, 'disable_existing_loggers': False,
+        'handlers': {  # Include the default django email handler for errors
+            # This is what you'd get without configuring logging at all.
+            'mail_admins': {
+                'class': 'django.utils.log.AdminEmailHandler',
+                'level': 'ERROR',
+                # But the emails are plain text by default - HTML is nicer
+                'include_html': True,
+            },
+            # Log to a text file that can be rotated by logrotate
+            'logfile': {
+                'class': 'logging.handlers.WatchedFileHandler', 'filename': '/applications/investment.log'
+            },
+        },
+        'loggers': {  # Again, default Django configuration to email unhandled exceptions
+            'django.request': {
+                'handlers': ['mail_admins'], 'level': 'ERROR',
+                'propagate': True,
+            },  # Might as well log any errors anywhere else in Django
+            'django': {
+                'handlers': ['logfile'], 'level': 'ERROR',
+                'propagate': False,
+            },
+            # Your own app - this assumes all your logger names start with "myapp."
+            'myapp': {
+                'handlers': ['logfile'],
+                'level': 'WARNING',  # Or maybe INFO or DEBUG
+                'propagate': False},
+        },
+    }
